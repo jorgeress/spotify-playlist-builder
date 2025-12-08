930910be927164269d7b4876663c2b6a23e79d55
jorgeress <162486102+jorgeress@users.noreply.github.com>
2025-12-08 15:05:41 +0100

    cambios

--- proyecto_final/src/components/widgets/ArtistWidget.jsx
+++ proyecto_final/src/components/widgets/ArtistWidget.jsx
@@ -1,20 +1,59 @@
 "use client";
 
-import { useEffect } from "react";
+import { useEffect, useState} from "react";
 
-async function getArtists(query){
-const response = await fetch(
-  `https://api.spotify.com/v1/search?type=artist&q=${query}`,
-  {
-    headers: {
-      Authorization: `Bearer ${accessToken}`
-    }
+export default  function ArtistWidget({token, selectedItems, onSelect}){
+
+  const [query, setQuery] = useState("");
+  const [results, setResults] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  // Debounce
+  useEffect(() => {
+    const delay = setTimeout(() => {
+      if (query.trim().length > 0) fetchArtists();
+    }, 400);
+    return () => clearTimeout(delay);
+  }, [query]);
+
+  async function fetchArtists(){
+    setLoading(true);
+    const res = fetch(`/api/search?type=artist&q=${query}`)
+    const data = (await res).json();
+    setResults(data.artists.items);
+    setLoading(false);
   }
-)
 
-const artist = response.json();
-console.log(artist)
-return artist;
+  function toggleArtist(artist){
+    onSelect(artist);
+  }
 
+  return (
+    <div className="widget-artist-container">
+      <input
+      type="text"
+      placeholder="Search artist..."
+      value={query}
+      onChange={(e) => setQuery(e.target.value)}
+      />
 
+      <div className="results">
+        {results.map((artist) => (
+          <div
+            key={artist.id}
+            className={`artist-card ${
+              selectedItems.some((a) => a.id === artist.id)
+                ? "selected"
+                : ""
+            }`}
+            onClick={() => toggleArtist(artist)}
+          >
+            <img src={artist.images?.[0]?.url} alt={artist.name} />
+            <p>{artist.name}</p>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 }
+    
